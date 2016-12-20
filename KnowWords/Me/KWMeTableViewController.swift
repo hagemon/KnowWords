92bd9bd302f825f6f9b9f0cee627c3972ae631de
oooneFolder <ooonefolder@gmail.com>
2016-12-20 12:17:21 +0800

    Finish Me module UI

---
+++ KnowWords/Me/KWMeTableViewController.swift
@@ -0,0 +1,94 @@
+//
+//  KWMeTableViewController.swift
+//  KnowWords
+//
+//  Created by 一折 on 2016/12/19.
+//  Copyright © 2016年 yizhe. All rights reserved.
+//
+
+import UIKit
+
+class KWMeTableViewController: UITableViewController {
+    
+    
+    let cellText = [["我的录音","我的历程","设置"],["清除缓存","退出登录"]]
+    let icons = [["myrecord","mytrip","setting"],["clear","logout"]]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.backgroundView = nil
+        self.tableView.backgroundColor = inf.backColor
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 2
+        case 1:
+            return 3
+        case 2:
+            return 2
+        default:
+            return 1
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.section {
+        case 0:
+            if indexPath.row == 0{return 150}
+            else{return 80}
+        case 1:
+            return 44
+        default:
+            return 44
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cellid = ""
+        switch indexPath.section {
+        case 0:
+            if indexPath.row == 0{cellid="headCell"}
+            else{cellid="nameCell"}
+        case 1:
+            cellid = "basicCell"
+        default:
+            cellid = "basicCell"
+        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellid)!
+        
+        if cellid == "headCell"{
+            let headView = cell.viewWithTag(1) as!UIImageView
+            headView.image = UIImage(named: "avatar")
+        }else if cellid == "nameCell"{
+            let usernameLabel = cell.viewWithTag(1) as!UILabel
+            usernameLabel.text = "Username"
+            let nicknameLabel = cell.viewWithTag(2) as!UILabel
+            nicknameLabel.text = "Nickname"
+        }else{
+            let index = indexPath.section-1
+            cell.textLabel?.text = self.cellText[index][indexPath.row]
+            cell.textLabel?.textColor = inf.fontColor
+            cell.imageView?.image = UIImage(named: self.icons[index][indexPath.row])
+        }
+        cell.backgroundColor = inf.backColor
+        return cell
+    }
+
+    
+}
